a5b9f056ee3efc888c359a897fc18256d9f38c14
Thomas Chia <thomas.h.chia@googlemail.com>
2020-11-23 16:34:32 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>pages/index.js</code>. Test.
         </p>
 
         <div className={styles.grid}>
